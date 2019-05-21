3ab384da221dfefa60dfb453121e93e411e647a9
Stone Tickle <lattis@mochiro.moe>
2019-05-21 14:27:16 -0500

    don't treat commands differently

--- util/compile.zsh
+++ util/compile.zsh
@@ -4,9 +4,4 @@ typeset -g sc_path="${${0:A:h}:h}"
 
 for file in "$sc_path"/src/**/*.zsh; do
   zcompile "$file"
-
-  if [[ "${file:a:h}" == "$sc_path/src/sc/cmds" ]]; then
-    mv -f "${file}.zwc" "${file:h}/cmd_${${file:t}%%.zsh}_.zwc"
-    continue
-  fi
 done
