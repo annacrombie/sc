544cd9a77e4874ce6601325f372c47aaf35db795
Stone Tickle <lattis@mochiro.moe>
2019-05-21 09:33:29 -0500

    use expiration dates in prune command

--- src/sc/cmds/prune.zsh
+++ src/sc/cmds/prune.zsh
@@ -2,11 +2,14 @@ cmd_prune_() {
   case $sc_trailing[1] in
     stale)
       # stale files
-      find "$sc_dirs[cache]" -type f -not -mmin 60 -exec rm '{}' +
+      find "$sc_dirs[cache]" -type f -not -mmin $((sc_expiration[cache]/60)) \
+        -exec echo '{}' +
+      find "$sc_dirs[cache]" -type l -not -mmin $((sc_expiration[resolve]/60)) \
+        -exec echo '{}' +
       ;;
-    links)
+    links | broken)
       # invalid links
-      find -L sc_data -type l
+      find -L "$sc_dirs[base]" -type l -exec echo '{}' +
       ;;
     *) die_ "what to prune?"
   esac
