206a1f79da11a807a4a68c0109a70762e2153b72
Stone Tickle <lattis@mochiro.moe>
2019-05-22 10:35:54 -0500

    fix prune command, don't prune tracks

--- src/sc/cmds/prune.zsh
+++ src/sc/cmds/prune.zsh
@@ -2,14 +2,28 @@ cmd_prune_() {
   case $sc_trailing[1] in
     stale)
       # stale files
-      find "$sc_dirs[cache]" -type f -not -mmin $((sc_expiration[cache]/60)) \
-        -exec rm '{}' +
-      find "$sc_dirs[cache]" -type l -not -mmin $((sc_expiration[resolve]/60)) \
-        -exec rm '{}' +
+      if [[ $sc_expiration[cache] != inf ]]; then
+        find "$sc_dirs[cache]" -type f -not -name 'stream$' \
+          -not -mmin $((sc_expiration[cache]/60)) \
+          -exec rm -v '{}' +
+      fi
+
+      if [[ $sc_expiration[resolve] != inf ]]; then
+        find "$sc_dirs[cache]" -type l \
+          -not -mmin $((sc_expiration[resolve]/60)) \
+          -exec rm -v '{}' +
+      fi
+
+      if [[ $sc_expiration[tracks] != inf ]]; then
+        find "$sc_dirs[cache]" -type f \
+          -name 'stream$' \
+          -not -mmin $((sc_expiration[tracks]/60)) \
+          -exec rm -v '{}' +
+      fi
       ;;
     links | broken)
       # invalid links
-      find -L "$sc_dirs[base]" -type l -exec rm '{}' +
+      find -L "$sc_dirs[base]" -type l -exec rm -v '{}' +
       ;;
     *) die_ "what to prune?"
   esac
