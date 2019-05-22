5356c0c87a3e0ea632a59a9061e1f585c374bb30
Stone Tickle <lattis@mochiro.moe>
2019-05-22 08:08:03 -0500

    add more detail to sc-env

--- src/sc/cmds/env.zsh
+++ src/sc/cmds/env.zsh
@@ -1,10 +1,17 @@
 cmd_env_() {
+  echo "package versions\n"
   find --version | head -n 1
-  git --version
   jq --version
   mktemp --version | head -n 1
+  ln --version | head -n 1
   tree --version
   which wcache
   zsh --version
-  echo "sc $sc_version"
+  echo "sc $sc_version master@$(cat $sc_path/.git/refs/heads/master)"
+
+  echo "\nsc state\n"
+
+  unset sc_key # don't output secret key
+  typeset -m 'sc_*' | sort
+  typeset -m PWD
 }
