4cf1e36146da160520dfe768dcb912a7196f826b
Stone Tickle <lattis@mochiro.moe>
2019-05-22 16:11:44 -0500

    add some display tests

--- test/offline.zsh
+++ test/offline.zsh
@@ -6,7 +6,14 @@ typeset -g user=earthlibraries
 typeset -a tests=(
   "sc_ resolve $user | jq -Mr '.followers'"  =    15
   "sc_ r $user | sc_ d | jq -Mr '.type'"     "=~" user
+  "sc_ r $user | sc_ --force-tty-out d"      "=~" 'type: user, permalink: earthlibraries
+  Earth Libraries
+  plan: Free / tracks: 8 / followers: 15
+  Hello Stuff Explorer! Welcome to Earth Libraries! A recently rediscovered govern'
   "sc_ r $user | sc_ t | sc_ c"              =    8
+  "sc_ r $user | sc_ --force-tty-out -t 1 t" =
+'title                       plays  permalink
+Dommel Mossel - Marco Polo  2      earthlibraries/dommel-mossel-marco-polo'
   "sc_ r $user | sc_ followers | sc_ c"      =    13
   "sc_ r $user | sc_ followings | sc_ c"     =    25
   "sc_ r $user | sc_ t | sc_ u | sc_ t | sc_ u | jq -Mr '.permalink'"
