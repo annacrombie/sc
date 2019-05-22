c8be22f842a5f1ae771d28010724f9dbf84d120b
Stone Tickle <lattis@mochiro.moe>
2019-05-22 15:59:35 -0500

    better env command

--- src/sc/cmds/env.zsh
+++ src/sc/cmds/env.zsh
@@ -1,18 +1,35 @@
 cmd_env_() {
-  echo "package versions\n"
-  find --version | head -n 1
-  jq --version
-  mktemp --version | head -n 1
-  ln --version | head -n 1
-  tree --version
-  zsh --version
-  column --version
-  echo "optparse master@$(cat "$sc_path"/deps/optparse/.git/refs/heads/master)"
-  echo "wcache master@$(cat "$sc_path"/deps/wcache/.git/refs/heads/master)"
-  echo "sc $sc_version master@$(cat "$sc_path"/.git/refs/heads/master)"
+  typeset -a bins=(find jq mktemp ln tree zsh column)
+  typeset bin
+  typeset missing=()
 
-  echo "\nsc state\n"
+  echo "required commands\n---"
 
+  for bin in ${(i)bins}; do
+    whence "$bin" >/dev/null
+
+    if [[ $? -eq 0 ]]; then
+      typeset ver=$("$bin" --version 2>/dev/null | head -n 1)
+      if [[ $? -eq 0 ]]; then
+        echo "\e[32m${bin}\e[0m found! (${ver})"
+      else
+        echo "\e[32m${bin}\e[0m found, but doesn't have --version arg"
+      fi
+    else
+      echo "\e[31m${bin}\e[0m not found"
+      missing+="$bin"
+    fi
+  done
+
+  if [[ $#missing -gt 0 ]]; then
+    echo "\e[31muh-oh\e[0m missing commands: ${missing}"
+  fi
+
+  echo "\ngit status\n---"
+  echo " $(git rev-parse HEAD) sc"
+  git submodule status --recursive
+
+  echo "\nenvironment\n---"
   [[ -n "$sc_key" ]] && sc_key=${sc_key//[[:alnum:]]/x}
 
   typeset -m 'sc_*' | sort
