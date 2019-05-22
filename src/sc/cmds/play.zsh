f1367159d37def064069e490bbee3000fd287057
Stone Tickle <lattis@mochiro.moe>
2019-05-22 11:39:22 -0500

    different behavior for non-piped sc play

--- src/sc/cmds/play.zsh
+++ src/sc/cmds/play.zsh
@@ -1,10 +1,6 @@
 cmd_play_() {
   if [[ ! $sc_pipe ]]; then
-    $sc_exec resolve $sc_trailing |\
-      $sc_exec tracks |\
-      $sc_exec fetch |\
-      $sc_exec play
-    return
+    exec mpv "${sc_dirs[tracks]}/${sc_trailing}"
   fi
 
   mktmp_
