478057fe5b12576b495de9de309da26f449640ec
Stone Tickle <lattis@mochiro.moe>
2019-06-03 15:14:29 -0500

    make the art command more robust

--- src/sc/cmds/art.zsh
+++ src/sc/cmds/art.zsh
@@ -1,13 +1,24 @@
-#display a track's art
+#display an object's art with feh
 
 cb_track_() {
   typeset -A track=($@)
+  fetch_art_ "${track[img]}" "${track[artist]} - ${track[title]}"
+}
+
+cb_user_() {
+  typeset -A user=($@)
+  fetch_art_ "${user[img]}" "${user[username]}"
+}
+
+fetch_art_() {
+  typeset url="$1"
+  typeset title="$2"
   typeset ret
 
-  [[ -z $track[img] ]] && return
+  [[ -z $url ]] && return
 
   wcache_ --best-by=$sc_expiration[cache] --cache=$sc_dirs[cache] \
-    $key --link-output -O "$tmpdir/${track[artist]} - ${track[title]}" "${track[img]}"
+    --link-output -O "$tmpdir/$title" "$url"
 }
 
 cmd_art_() {
@@ -18,7 +29,7 @@ cmd_art_() {
   mktmp_ -d
   typeset -g tmpdir="$returned"
 
-  eval_loop_ track cb_track_
+  eval_loop_ track cb_track_ user cb_user_
 
   feh -. "$tmpdir"
 }
